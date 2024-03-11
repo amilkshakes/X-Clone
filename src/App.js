2e9f81038252608d9d5171e349897392d431e7b3
Ayaan Shaikh <reachayaans@gmail.com>
2024-03-11 14:12:55 +0530

    Removed Unwanted Files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <h1>
+    .... Under Construction ....   
+    </h1>  
+    
+    <h2>
+     X Clone    
+    </h2> 
+    
+    </div>
+  );
+}
+
+export default App;
